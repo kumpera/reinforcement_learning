6640469a37a01aa3712faecd72d802ebe2c0fbc1
Rodrigo Kumpera <kumpera@gmail.com>
2021-01-12 12:24:28 -0500

    prototype of binary joiner.

--- test_tools/log_parser/joiner.py
+++ test_tools/log_parser/joiner.py
@@ -1,83 +1,44 @@
 #! /usr/bin/env python3 -W ignore::DeprecationWarning
-import reinforcement_learning.messages.flatbuff.v2.CbEvent as cb
-import reinforcement_learning.messages.flatbuff.v2.EventBatch as eb
-import reinforcement_learning.messages.flatbuff.v2.DedupInfo as di
-import reinforcement_learning.messages.flatbuff.v2.PayloadType as pt
-import reinforcement_learning.messages.flatbuff.v2.LearningModeType as lm
+from reinforcement_learning.messages.flatbuff.v2.EventBatch import *
+from reinforcement_learning.messages.flatbuff.v2.BatchMetadata import *
+from reinforcement_learning.messages.flatbuff.v2.EventBatch import *
+from reinforcement_learning.messages.flatbuff.v2.LearningModeType import LearningModeType
+from reinforcement_learning.messages.flatbuff.v2.PayloadType import PayloadType
+from reinforcement_learning.messages.flatbuff.v2.OutcomeValue import OutcomeValue
+from reinforcement_learning.messages.flatbuff.v2.NumericOutcome import NumericOutcome
+from reinforcement_learning.messages.flatbuff.v2.NumericIndex import NumericIndex
+
+from reinforcement_learning.messages.flatbuff.v2.CbEvent import CbEvent
+from reinforcement_learning.messages.flatbuff.v2.OutcomeEvent import OutcomeEvent
+from reinforcement_learning.messages.flatbuff.v2.MultiSlotEvent import MultiSlotEvent
+from reinforcement_learning.messages.flatbuff.v2.CaEvent import CaEvent
+from reinforcement_learning.messages.flatbuff.v2.DedupInfo import DedupInfo
+
+from reinforcement_learning.messages.flatbuff.v2.KeyValue import *  
+from reinforcement_learning.messages.flatbuff.v2.TimeStamp import *  
+from reinforcement_learning.messages.flatbuff.v2.FileHeader import *  
+from reinforcement_learning.messages.flatbuff.v2.SerializedBatch import *  
+from reinforcement_learning.messages.flatbuff.v2.JoinedPayload import *  
+from reinforcement_learning.messages.flatbuff.v2.BatchType import *  
+from reinforcement_learning.messages.flatbuff.v2.Metadata import *  
+from reinforcement_learning.messages.flatbuff.v2.Event import *  
+
+
 import flatbuffers
 import zstandard as zstd
 import sys
 import json
+import struct
+from datetime import datetime
+import numpy as np
 
-# PREAMBLE_LENGTH = 8
+"""
+TODO:
+Incremental join instead of loading all interactions at once
+Respect EUD.
+"""
 
-# def parse_preamble(buf):
-#     reserved = buf[0]
-#     version = buf[1]
-#     msg_type = int.from_bytes(buf[2:4], "big")
-#     msg_size = int.from_bytes(buf[4:8], "big")
-#     return { 'reserved': reserved, 'version': version, 'msg_type': msg_type, 'msg_size': msg_size}
-
-# def payload_name(payload):
-#     for k in [f for f in dir(pt.PayloadType) if not f.startswith('__')]:
-#         if getattr(pt.PayloadType, k) == payload:
-#             return k
-#     return f'<unk_{payload}>'
-
-# def learning_mode_name(learning_mode):
-#     for k in [f for f in dir(lm.LearningModeType) if not f.startswith('__')]:
-#         if getattr(lm.LearningModeType, k) == learning_mode:
-#             return k
-#     return f'<unk_{learning_mode}>'
-
-
-# def process_payload(payload, is_dedup):
-#     if not is_dedup:
-#         return payload
-#     return zstd.decompress(payload)
-
-# def parse_cb(payload):
-#     evt = cb.CbEvent.GetRootAsCbEvent(payload, 0)
-#     print(f'\tcb: actions:{evt.ActionIdsLength()} ctx:{evt.ContextLength()} probs:{evt.ProbabilitiesLength()} lm:{learning_mode_name(evt.LearningMode())}')
-#     payload = json.loads(bytearray(evt.ContextAsNumpy()).decode('utf-8'))
-#     print('\tcontext:')
-#     print(json.dumps(payload, indent = 1))
-
-# def parse_action_dict(payload):
-#     evt = di.DedupInfo.GetRootAsDedupInfo(payload, 0)
-#     print(f'\t\tad: ids:{evt.IdsLength()} values:{evt.ValuesLength()}')
-#     for i in range(0, evt.ValuesLength()):
-#         print(f'\t\t\t[{evt.Ids(i)}]: "{evt.Values(i)}"')
-
-# def dump_event(evt, idx, is_dedup):
-#     m = evt.Meta()
-#     print(f'\t[{idx}] id:{m.Id()} type:{payload_name(m.PayloadType())} payload-size:{evt.PayloadLength()}')
-
-#     payload = process_payload(evt.PayloadAsNumpy(), is_dedup)
-
-#     if m.PayloadType() == pt.PayloadType.DedupInfo:
-#         parse_action_dict(payload)
-#     elif m.PayloadType() == pt.PayloadType.CB:
-#         parse_cb(payload)
-
-
-# def dump_file(f):
-#     buf = open(f, 'rb').read()
-#     buf = bytearray(buf)
-#     preamble = parse_preamble(buf)
-
-#     batch = eb.EventBatch.GetRootAsEventBatch(buf[PREAMBLE_LENGTH : PREAMBLE_LENGTH + preamble["msg_size"]], 0)
-#     meta = batch.Metadata()
-#     print(f'parsed {f} with {batch.EventsLength()} events preamble:{preamble} enc:{meta.ContentEncoding()}')
-#     is_dedup = b'ZSTD_AND_DEDUP' == meta.ContentEncoding()
-#     for i in range(0, batch.EventsLength()):
-#         dump_event(batch.Events(i), i, is_dedup)
-#     print("----\n")
-
-# for f in sys.argv[1:]:
-#     dump_file(f)
-
-class PreambleStream:
+class PreambleStreamReader:
     def __init__(self, file_name):
         self.file = open(file_name, 'rb')
     
@@ -98,33 +59,187 @@ class PreambleStream:
             if header == None:
                 break
             msg = self.file.read(header['msg_size'])
-            yield eb.EventBatch.GetRootAsEventBatch(msg, 0)
+            # yield (EventBatch.GetRootAsEventBatch(msg, 0), msg)
+            yield msg
 
 
-"""
-TODO:
-Incremental join instead of loading all interactions at once
-Respect EUD.
-"""
-interactions_file = PreambleStream('iter.fb')
-observations_file = PreambleStream('obs.fb')
+def mk_weirdtime(builder, time=None, weirdTime=None):
+    if weirdTime != None:
+        return CreateTimeStamp(builder, weirdTime.Year(), weirdTime.Month(), weirdTime.Day(), weirdTime.Hour(), weirdTime.Minute(), weirdTime.Second(), weirdTime.Subsecond())
+
+    if time == None:
+        time = datetime.utcnow()
+    return CreateTimeStamp(builder, time.year, time.month, time.day, time.hour, time.minute, time.second, int(time.microsecond))
+
+def mk_serialized_batch(builder, kind, payload):
+    off = mk_bytes_vector(builder, payload)
+
+    SerializedBatchStart(builder)
+    SerializedBatchAddType(builder, kind)
+    SerializedBatchAddPayload(builder, off)
+
+    return SerializedBatchEnd(builder)
+
+def create_string_or_none(builder, string):
+    if string != None:
+        return builder.CreateString(str(string))
+    return None
+
+def reserialize_event(builder, evt):
+    md_off = None
+    meta = evt.Meta()
+    if meta != None:
+        evt_id = create_string_or_none(builder, meta.Id())
+        app_id = create_string_or_none(builder, meta.AppId())
+
+        MetadataStart(builder)
+        if evt_id:
+            MetadataAddId(builder, evt_id)
+        MetadataAddClientTimeUtc(builder, mk_weirdtime(builder, weirdTime=meta.ClientTimeUtc()))
+        if app_id:
+            MetadataAddAppId(builder, app_id)
+        MetadataAddPayloadType(builder, meta.PayloadType())
+        MetadataAddPassProbability(builder, meta.PassProbability())
+        md_off = MetadataEnd(builder)
+
+    payload_off = mk_bytes_vector(builder, evt.PayloadAsNumpy())
+
+    EventStart(builder)
+    EventAddMeta(builder, md_off)
+    EventAddPayload(builder, payload_off)
+    return EventEnd(builder)
+
+def mk_offsets_vector(builder, arr, startFun):
+    startFun(builder, len(arr))
+    for i in reversed(range(len(arr))):
+        builder.PrependUOffsetTRelative(arr[i])
+    return builder.EndVector(len(arr))
+
+def mk_bytes_vector(builder, arr):
+    return builder.CreateNumpyVector(np.array(list(arr), dtype='b'))
+
+MSG_TYPE_HEADER = 0x55555555
+MSG_TYPE_REGULAR = 0xFFFFFFFF
+MSG_TYPE_EOF = 0xAAAAAAAA
+
+class BinLogWriter:
+    def __init__(self, file_name):
+        self.file = open(file_name, 'wb')
+    
+    def write_message(self, kind, payload):
+        padding_bytes = len(payload) % 8
+        print(f'msg {kind:X} size: {len(payload)} padding {padding_bytes}')
+
+        self.file.write(struct.pack('I', kind))
+        self.file.write(struct.pack('I', len(payload)))
+        self.file.write(payload)
+        if padding_bytes > 0:
+            self.file.write(bytes([0] * padding_bytes))
+
+    def write_header(self, properties):
+        self.file.write(b'VWFB')
+        self.file.write(struct.pack('I', 1))
+
+        builder = flatbuffers.Builder(0)
+        kv_offsets = []
+        for key in properties:
+            value = properties[key]
+            k_off = builder.CreateString(str(key))
+            v_off = builder.CreateString(str(value))
+            KeyValueStart(builder)
+            KeyValueAddKey(builder, k_off)
+            KeyValueAddValue(builder, v_off)
+            kv_offsets.append(KeyValueEnd(builder))
+
+        props_off = mk_offsets_vector(builder, kv_offsets, FileHeaderStartPropertiesVector)
+
+        FileHeaderStart(builder)
+        FileHeaderAddJoinTime(builder, mk_weirdtime(builder))
+        FileHeaderAddProperties(builder, props_off)
+
+        header_off = FileHeaderEnd(builder)
+        builder.Finish(header_off)
+        self.write_message(MSG_TYPE_HEADER, builder.Output())
+
+    def write_join_msg(self, iteractions_bytes, obs):
+        builder = flatbuffers.Builder(0)
+
+        iter_off = mk_serialized_batch(builder, BatchType.Interactions, iteractions_bytes)
+
+        #create an observation batch
+        b2 = flatbuffers.Builder(0)
+        evt_offs = []
+        for evt_lst in obs:
+            for evt in evt_lst:
+                evt_offs.append(reserialize_event(b2, evt))
+
+        evts_off = mk_offsets_vector(b2, evt_offs, EventBatchStartEventsVector)
+
+        content_encoding_off = b2.CreateString("IDENTITY")
+        BatchMetadataStart(b2)
+        BatchMetadataAddContentEncoding(b2, content_encoding_off)
+        md_off = BatchMetadataEnd(b2)
+        
+        EventBatchStart(b2)
+        #TODO batch metadata
+        EventBatchAddMetadata(b2, md_off)
+        EventBatchAddEvents(b2, evts_off)
+        b2.Finish(EventBatchEnd(b2))
+
+        obs_off = mk_serialized_batch(builder, BatchType.Observations, b2.Output())
+
+        batches_off = mk_offsets_vector(builder, [iter_off, obs_off], JoinedPayloadStartBatchesVector)
+
+        JoinedPayloadStart(builder)
+        JoinedPayloadAddBatches(builder, batches_off)
+        builder.Finish(JoinedPayloadEnd(builder))
+
+        self.write_message(MSG_TYPE_REGULAR, builder.Output())
+
+    def write_eof(self):
+        self.write_message(MSG_TYPE_EOF, b'')
+
+
+# interactions_file = PreambleStreamReader('iter.fb')
+# observations_file = PreambleStreamReader('obs.fb')
+interactions_file = PreambleStreamReader('large_it.fb')
+observations_file = PreambleStreamReader('large_obs.fb')
 # in_f = open(interactions_file, "rb")
-result_file = 'merged.fb'
+result_file = 'merged.log'
 
 observations = dict()
-for batch in observations_file.messages():
+obs_count = 0
+obs_ids = 0
+for msg in observations_file.messages():
+    batch = EventBatch.GetRootAsEventBatch(msg, 0)
     for i in range(0, batch.EventsLength()):
         evt = batch.Events(i)
         # print(f'got evt {evt.Meta().Id()} kind:{evt.Meta().PayloadType()}')
         evt_id = evt.Meta().Id()
         if evt_id not in observations:
             observations[evt_id] = []
+            obs_ids += 1
         observations[evt_id].append(evt)
+        obs_count +=1
 
-for batch in interactions_file.messages():
+print(f'found {obs_count} observations with {obs_ids} ids')
+# join_streams(interactions_file observations_file, result_file)
+bin_f = BinLogWriter(result_file)
+bin_f.write_header({ 'eud': '-1', 'joiner': 'joiner.py', 'reward': 'latest'})
+
+for msg in interactions_file.messages():
+    batch = EventBatch.GetRootAsEventBatch(msg, 0)
+    #collect all observations
+    used_obs = 0
+    obs = []
     for i in range(0, batch.EventsLength()):
         evt = batch.Events(i)
         evt_id = evt.Meta().Id()
-        print(f'evt {evt_id} has_reward:{evt_id in observations}')
+        if evt_id in observations:
+            obs.append(observations[evt_id])
+            used_obs += len(observations[evt_id])
+    print(f'batch with {len(obs)} iteractions and {used_obs} observations')
+    print(f'joining iters with {batch.Metadata().ContentEncoding()} len:{len(msg)}')
+    bin_f.write_join_msg(msg, obs)
 
-# join_streams(interactions_file observations_file, result_file)
+bin_f.write_eof()
--- test_tools/log_parser/parser.py
+++ test_tools/log_parser/parser.py
@@ -3,6 +3,7 @@ import flatbuffers
 import zstd
 import sys
 import json
+import struct
 
 PREAMBLE_LENGTH = 8
 PRETTY_PRINT_JSON=False
@@ -21,17 +22,21 @@ def parse_preamble(buf):
     msg_size = int.from_bytes(buf[4:8], "big")
     return { 'reserved': reserved, 'version': version, 'msg_type': msg_type, 'msg_size': msg_size}
 
-def payload_name(payload):
-    for k in [f for f in dir(PayloadType) if not f.startswith('__')]:
-        if getattr(PayloadType, k) == payload:
+
+def enum_to_str(type, value):
+    for k in [f for f in dir(type) if not f.startswith('__')]:
+        if getattr(type, k) == value:
             return k
-    return f'<unk_{payload}>'
+    return f'<unk_{value}>'
+
+def payload_name(payload):
+    return enum_to_str(PayloadType, payload)
 
 def learning_mode_name(learning_mode):
-    for k in [f for f in dir(LearningModeType) if not f.startswith('__')]:
-        if getattr(LearningModeType, k) == learning_mode:
-            return k
-    return f'<unk_{learning_mode}>'
+    return enum_to_str(LearningModeType, learning_mode)
+
+def batch_type_name(batch_type):
+    return enum_to_str(BatchType, batch_type)
 
 def process_payload(payload, is_dedup):
     if not is_dedup:
@@ -110,18 +115,99 @@ def dump_event(evt, idx, is_dedup):
     else:
         print('unknown payload type')
 
-def dump_file(f):
-    buf = open(f, 'rb').read()
-    buf = bytearray(buf)
-    preamble = parse_preamble(buf)
 
-    batch = EventBatch.GetRootAsEventBatch(buf[PREAMBLE_LENGTH : PREAMBLE_LENGTH + preamble["msg_size"]], 0)
+def dump_event_batch(buf):
+    batch = EventBatch.GetRootAsEventBatch(buf, 0)
     meta = batch.Metadata()
-    print(f'parsed {f} with {batch.EventsLength()} events preamble:{preamble} enc:{meta.ContentEncoding()}')
+    enc = meta.ContentEncoding().decode('utf-8')
+    print(f'event-batch evt-count:{batch.EventsLength()} enc:{enc}')
     is_dedup = b'ZSTD_AND_DEDUP' == meta.ContentEncoding()
     for i in range(0, batch.EventsLength()):
         dump_event(batch.Events(i), i, is_dedup)
     print("----\n")
+
+
+def dump_preamble_file(file_name, buf):
+    preamble = parse_preamble(buf)
+    print(f'parsing preamble file {file_name} preamble:{preamble}')
+    dump_event_batch(buf[PREAMBLE_LENGTH : PREAMBLE_LENGTH + preamble["msg_size"]])
+
+MSG_TYPE_HEADER = 0x55555555
+MSG_TYPE_REGULAR = 0xFFFFFFFF
+MSG_TYPE_EOF = 0xAAAAAAAA
+
+class JoinedLogStreamReader:
+    def __init__(self, buf):
+        self.buf = buf
+        self.offset = 0
+        self.headers = dict()
+        self.parse_header()
+        self.read_file_header()
+    
+    def parse_header(self):
+        if self.buf[0:4] != b'VWFB':
+            raise Exception("Invalid file magic")
+
+        self.version = struct.unpack('I', self.buf[4:8])[0]
+        if self.version != 1:
+            raise Exception(f'Unsuported file version {self.version}')
+        
+        self.offset = 8
+
+    def read(self, size):
+        if size == 0:
+            return bytearray([])
+        data = self.buf[self.offset : self.offset + size]
+        self.offset += size
+        return data
+
+    def read_message(self):
+        kind = struct.unpack('I', self.read(4))[0]
+        length = struct.unpack('I', self.read(4))[0]
+        payload = self.read(length)
+        #discard padding
+        self.read(length % 8)
+        return (kind, payload)
+    
+    def read_file_header(self):
+        msg = self.read_message()
+        if msg[0] != MSG_TYPE_HEADER:
+            raise f'Missing file header, found message of type {msg[0]} instead'
+
+        header = FileHeader.GetRootAsFileHeader(msg[1], 0)
+        for i in range(header.PropertiesLength()):
+            p = header.Properties(i)
+            self.headers[p.Key().decode('utf-8')] = p.Value().decode('utf-8')
+
+    def messages(self):
+        while True:
+            msg = self.read_message()
+            if msg[0] == MSG_TYPE_EOF:
+                break
+            yield JoinedPayload.GetRootAsJoinedPayload(msg[1], 0)
+
+def dump_joined_log_file(file_name, buf):
+    reader = JoinedLogStreamReader(buf)
+    print(f'parsing joined log:{file_name} header:')
+    for k in reader.headers:
+        print(f'\t{k} = {reader.headers[k]}')
+
+    for msg in reader.messages():
+        print(f'joined-batch count:{msg.BatchesLength()}')
+        for i in range(msg.BatchesLength()):
+            batch = msg.Batches(i)
+            print(f'ser-batch [{i}] kind:{batch_type_name(batch.Type())} size:{batch.PayloadLength()}')
+            dump_event_batch(batch.PayloadAsNumpy())
+            
+
+def dump_file(f):
+    buf = bytearray(open(f, 'rb').read())
+
+    if buf[0:4] == b'VWFB':
+        dump_joined_log_file(f, buf)
+    else:
+        dump_preamble_file(f, buf)
+
 
 # Generate FB serializers if they are not available
 try:
@@ -150,6 +236,9 @@ from reinforcement_learning.messages.flatbuff.v2.OutcomeEvent import OutcomeEven
 from reinforcement_learning.messages.flatbuff.v2.MultiSlotEvent import MultiSlotEvent
 from reinforcement_learning.messages.flatbuff.v2.CaEvent import CaEvent
 from reinforcement_learning.messages.flatbuff.v2.DedupInfo import DedupInfo
+from reinforcement_learning.messages.flatbuff.v2.FileHeader import FileHeader
+from reinforcement_learning.messages.flatbuff.v2.JoinedPayload import JoinedPayload
+from reinforcement_learning.messages.flatbuff.v2.BatchType import BatchType
 
-for f in sys.argv[1:]:
-    dump_file(f)
+for input_file in sys.argv[1:]:
+    dump_file(input_file)
